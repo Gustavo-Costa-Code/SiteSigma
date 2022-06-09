a505c06e33c471261990056ad5748804dc76be35
Gustavo Costa <79706058+Gustavo-Costa-Code@users.noreply.github.com>
2022-06-09 12:19:05 -0300

    Add files via upload

--- sitesigma/includes/_dados.php
+++ sitesigma/includes/_dados.php
@@ -1,7 +1,16 @@
 <?php
 
 $produtos = array(
-    0 => array('nome'=>'Capivara','preco'=>'R$1.000','descricao'=>'Uma capivara NFT','imagem'=>'capivara-nft.jpg' )
+    0 => array('nome'=>'Capivara','preco'=>'R$1.000','descricao'=>'Uma capivara NFT','imagem'=>'capivara_nft.jpg'),
+    1 => array('nome'=>'Elefante','preco'=>'R$2.000','descricao'=>'Um elefante NFT','imagem'=>'elefante_nft.jpg'),
+    2 => array('nome'=>'Girafa','preco'=>'R$500','descricao'=>'Uma girafa NFT','imagem'=>'girafa_nft.jpg' ),
+    3 => array('nome'=>'Capivara','preco'=>'R$1.000','descricao'=>'Uma capivara NFT','imagem'=>'capivara_nft.jpg' ),
+    4 => array('nome'=>'Capivara','preco'=>'R$1.000','descricao'=>'Uma capivara NFT','imagem'=>'capivara_nft.jpg' ),
+    5 => array('nome'=>'Capivara','preco'=>'R$1.000','descricao'=>'Uma capivara NFT','imagem'=>'capivara_nft.jpg' ),
+    6 => array('nome'=>'Capivara','preco'=>'R$1.000','descricao'=>'Uma capivara NFT','imagem'=>'capivara_nft.jpg' ),
+    7 => array('nome'=>'Capivara','preco'=>'R$1.000','descricao'=>'Uma capivara NFT','imagem'=>'capivara_nft.jpg' ),
+    8 => array('nome'=>'Capivara','preco'=>'R$1.000','descricao'=>'Uma capivara NFT','imagem'=>'capivara_nft.jpg' ),
+    9 => array('nome'=>'Capivara','preco'=>'R$1.000','descricao'=>'Uma capivara NFT','imagem'=>'capivara_nft.jpg' )
 );
 
 ?>
--- sitesigma/index.php
+++ sitesigma/index.php
@@ -3,9 +3,23 @@
 include_once './includes/_dados.php';
 include_once './includes/_head.php';
 include_once './includes/_header.php';
+
+$id = $_GET['id'];
+
 ?>
 
 <h1>Home</h1>
+<?php 
+    for($i=0;$i<3;$i++){
+        echo($produtos[$id]['nome'].' - ');
+        echo($produtos[$id]['preco'].' - ');
+        echo($produtos[$id]['descricao'].' - ');
+        ?>
+        <img src="./content/<?php echo $produtos[$i]['imagem'];?>" width='50px' height='50px'>
+        <?php 
+        echo '<hr>';
+    }
+?>
 
 <?php
 // include do footer
